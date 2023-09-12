03c961fe38272d0c3ad0676d55f7ef76065b8d11
Sorokin Nikita <sorokin.nik@live.com>
2023-09-12 07:03:42 +0300

    add 2 sections

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,18 +1,45 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     const promo = document.querySelector('.promo'),
-          plane = promo.querySelector('.promo__plane');
+          plane = promo.querySelector('.promo__plane'),
+          promoTracer = promo.querySelector('.promo__tracer');
+
+
+//==========ФУНКЦИЯ ДЛЯ СМЕЩЕНИЯ ЭЛЕМЕНТА ПРИ СКРОЛЛЕ ДОКУМЕНТА==========
+
+  const addParallax = (relocatableElem, defaultTop, defaultLeft, stepTop, stepLeft) => {
+
+    let oldScrollTopPosition = 0;
 
     window.addEventListener('scroll', () => {
-        let defaultTop = 0,
-            defaultLeft = 0;
 
-            defaultTop  = defaultTop - 2;
-            defaultLeft = defaultLeft + 1;
+        const scrollTopPosition = document.documentElement.scrollTop;
 
-            // plane.style.transform = `translate(${defaultLeft}rem, ${defaultTop}rem)`;
+        if(oldScrollTopPosition < scrollTopPosition) {
+            relocatableElem.classList.remove('add-transition');
+            defaultTop  = defaultTop - stepTop;
+            defaultLeft = defaultLeft + stepLeft;
+        }
 
-            console.log(plane.getBoundingClientRect());
+        if(oldScrollTopPosition > scrollTopPosition) {
+            defaultTop  = defaultTop + stepTop;
+            defaultLeft = defaultLeft - stepLeft;
+
+            if(scrollY === 0 || defaultTop < 0 || defaultLeft < 0){
+                defaultTop = 0;
+                defaultLeft = 0;
+                relocatableElem.classList.add('add-transition');
+            }
+        }
+        
+        relocatableElem.style.transform = `translate(${defaultLeft}rem, ${defaultTop}rem)`;
+
+        oldScrollTopPosition = scrollTopPosition;
     });
+  }
 
-    
+    addParallax(plane,0, 0, 1, 2);
+    addParallax(promoTracer,0, 0, 1, 2);
+
+// window.onwheel = (e) => console.log(e.deltaY < 0);
+  
 })
